038b9e99c890039829ab6590461e8925e1a939ef
Carlos Vinicius <viniciusdev.26@gmail.com>
2021-10-21 15:18:20 -0300

    feat: add ts-node-dev on project

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,9 @@ import express from "express";
 const app = express();
 
 app.get("/", (request, response) => {
-  return response.json("Hello World");
+  return response.json({ message: "Hello World" });
 });
 
-app.listen(3333, () => console.log("server is running on port 3333"));
+app.listen(3333, () =>
+  console.log("server is running on http://localhost:3333")
+);
