5418734f7010061a44880b6dcb7050cdc6c85fea
Carlos Vinicius <viniciusdev.26@gmail.com>
2021-10-21 18:39:39 -0300

    feat: add category routes

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
 import express from "express";
 
+import { categoriesRoutes } from "./routes/categories.routes";
+
 const app = express();
 
 app.use(express.json());
+app.use("/categories", categoriesRoutes);
 
 app.post("/courses", (request, response) => {
   const { name } = request.body;
