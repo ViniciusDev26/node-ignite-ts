9bb5fdc440bfd6c20a23bb0af1ee9f29b8736309
Carlos Vinicius <viniciusdev.26@gmail.com>
2021-10-21 20:34:31 -0300

    feat: create CategoriesRepository.ts

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,24 +1,27 @@
 import { Router } from "express";
 
-import { Category } from "../model/Category";
+import { CategoriesRepository } from "../repositories/CategoriesRepository";
 
 const categoriesRoutes = Router();
+const categoriesRepository = new CategoriesRepository();
 
-const categories: Category[] = [];
+categoriesRoutes.get("/", (request, response) => {
+  const allCategories = categoriesRepository.list();
+
+  return response.json({ allCategories });
+});
 
 categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
 
-  const category = new Category();
-  Object.assign(category, {
-    name,
-    description,
-    created_at: new Date(),
-  });
+  const categoryAlreadyExists = categoriesRepository.findByName(name);
 
-  categories.push(category);
+  if (categoryAlreadyExists)
+    return response.status(400).json({ error: "Category Already Exists!" });
 
-  return response.status(201).json(category);
+  categoriesRepository.create({ name, description });
+
+  return response.status(201).send();
 });
 
 export { categoriesRoutes };
