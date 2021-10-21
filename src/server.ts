254cd7ca84e3f84627d22e95761853fe5f6715c1
Carlos Vinicius <viniciusdev.26@gmail.com>
2021-10-21 15:08:34 -0300

    fix: change wrong config eslint

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
 
 app.get("/", (request, response) => {
   return response.json("Hello World");
-})
+});
 
-app.listen(3333);
+app.listen(3333, () => console.log("server is running on port 3333"));
