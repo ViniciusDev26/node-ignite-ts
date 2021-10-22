9bba301472617d9e45a3b711227640ea290cd704
Carlos Vinicius <viniciusdev.26@gmail.com>
2021-10-22 00:25:36 -0300

    feat: create use case to create category

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 
 import { CategoriesRepository } from "../modules/cars/repositories/CategoriesRepository";
-import { CreateCategoryService } from "../modules/cars/services/CreateCategoryService";
+import { createCategoryController } from "../modules/cars/useCases/createCategory";
 
 const categoriesRoutes = Router();
 const categoriesRepository = new CategoriesRepository();
@@ -13,15 +13,7 @@ categoriesRoutes.get("/", (request, response) => {
 });
 
 categoriesRoutes.post("/", (request, response) => {
-  const { name, description } = request.body;
-
-  const createCategoryService = new CreateCategoryService(categoriesRepository);
-  createCategoryService.execute({
-    name,
-    description,
-  });
-
-  return response.status(201).send();
+  return createCategoryController.handle(request, response);
 });
 
 export { categoriesRoutes };
