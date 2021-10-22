4f7bff60ddd9de7f186f7d6eac015403cd02819e
Carlos Vinicius <viniciusdev.26@gmail.com>
2021-10-22 01:36:02 -0300

    feat: implements CreateSpecificationUseCase

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,29 +1,16 @@
 import { Router } from "express";
 
-import { SpecificationsRepository } from "../modules/cars/repositories/SpecificationsRepository";
-import { CreateSpecificationService } from "../modules/cars/services/CreateSpecificationService";
+import { createSpecificationController } from "../modules/cars/useCases/createSpecification";
 
 const specificationsRoutes = Router();
-const specificationsRepository = new SpecificationsRepository();
 
 specificationsRoutes.get("/", (request, response) => {
-  const allSpecifications = specificationsRepository.list();
-  return response.json(allSpecifications);
+  // const allSpecifications = specificationsRepository.list();
+  // return response.json(allSpecifications);
 });
 
 specificationsRoutes.post("/", (request, response) => {
-  const { name, description } = request.body;
-
-  const createSpecificationService = new CreateSpecificationService(
-    specificationsRepository
-  );
-
-  createSpecificationService.execute({
-    name,
-    description,
-  });
-
-  return response.status(201).send();
+  return createSpecificationController.handle(request, response);
 });
 
 export { specificationsRoutes };
