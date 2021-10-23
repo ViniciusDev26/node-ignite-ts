ad131c136a0614e2e30eb28f4a62b17d921c77ed
Carlos Vinicius <viniciusdev.26@gmail.com>
2021-10-23 20:11:27 -0300

    feat: add function to remove file after read

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -24,6 +24,7 @@ class ImportCategoryUseCase {
           categories.push({ name, description });
         })
         .on("end", () => {
+          fs.promises.unlink(file.path);
           return resolve(categories);
         })
         .on("error", (err) => {
