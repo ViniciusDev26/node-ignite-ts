12eb65eec0b08d184fcc94e49b88a2ba2bfa7dbf
Carlos Vinicius <viniciusdev.26@gmail.com>
2021-10-23 02:56:12 -0300

    refactor: change routes implementations

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,11 @@
 import express from "express";
 
-import { categoriesRoutes } from "./routes/categories.routes";
-import { specificationsRoutes } from "./routes/specifications.routes";
+import { router } from "./routes";
 
 const app = express();
 
 app.use(express.json());
-app.use("/categories", categoriesRoutes);
-app.use("/specifications", specificationsRoutes);
-
-app.post("/courses", (request, response) => {
-  const { name } = request.body;
-
-  return response.json({ message: name });
-});
+app.use(router);
 
 app.listen(3333, () =>
   console.log("server is running on http://localhost:3333")
