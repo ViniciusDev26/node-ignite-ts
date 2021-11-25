670c74725452cbff8fdefbef02624a7835c1c450
Carlos Vinicius <viniciusdev.26@gmail.com>
2021-11-25 17:01:27 -0300

    feat: create middleware to use express-async-errors and manager responses if exception during execution

---
+++ src/middlewares/handleErrorsMiddleware.ts
@@ -0,0 +1,24 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { NextFunction, Request, Response } from "express";
+
+import { AppError } from "../errors/AppError";
+
+function handleErrorsMiddleware(
+  err: Error,
+  request: Request,
+  response: Response,
+  next: NextFunction
+) {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      message: err.message,
+    });
+  }
+
+  return response.status(500).json({
+    status: "error",
+    message: `Internal server error - ${err.message}`,
+  });
+}
+
+export { handleErrorsMiddleware };
--- src/server.ts
+++ src/server.ts
@@ -4,8 +4,10 @@ import "./database";
 import "./shared/container";
 
 import express from "express";
+import "express-async-errors";
 import swaggerUI from "swagger-ui-express";
 
+import { handleErrorsMiddleware } from "./middlewares/handleErrorsMiddleware";
 import { router } from "./routes";
 import swaggerFile from "./swagger.json";
 
@@ -15,6 +17,8 @@ app.use(express.json());
 app.use("/documentation", swaggerUI.serve, swaggerUI.setup(swaggerFile));
 
 app.use(router);
+
+app.use(handleErrorsMiddleware);
 
 app.listen(3333, () =>
   console.log("server is running on http://localhost:3333")
