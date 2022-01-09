2b9c1138f4266014a2e11ffca78478eeb8a7a001
Carlos Vinicius <viniciusdev.26@gmail.com>
2022-01-09 18:00:29 -0300

    config: add config to jest read the env file

--- jest.config.ts
+++ jest.config.ts
@@ -2,6 +2,7 @@
  * For a detailed explanation regarding each configuration property and type check, visit:
  * https://jestjs.io/docs/configuration
  */
+import "dotenv/config";
 
 export default {
   // All imported modules in your tests should be mocked automatically
